ffa9c838f47cd4fa83682b2c5751207b5a093056
sysdream <d.cauquil@sysdream.com>
2012-12-28 16:45:23 +0100

    Added a textview with a specific id inside the example activity

--- src/com/example/fino/MainActivity.java
+++ src/com/example/fino/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.fino;
 
+import java.util.*;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity
 {
+    private ArrayList<String> mList = null;
+    private HashMap<String, String> mHashmap = null;
+    private Vector<String> mVector = null;
+
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        /* Initialize test vector, hashmap and list */
+        mList = new ArrayList<String>();
+        mList.add("Hello");
+        mList.add("Kitty");
+        mHashmap = new HashMap<String, String>();
+        mHashmap.put("Lancelot","Green");
+        mHashmap.put("Galahad","Red");
+        mHashmap.put("Arthur","Purple");
+        mVector = new Vector<String>();
+        mVector.add("Swallow");
+        mVector.add("Coconut");
     }
 }
